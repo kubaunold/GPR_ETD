6b6bd260e3b788a0c904e2dae36832dc784b5a5b
Kuba Unold <jakub@unold.pl>
2020-06-18 12:24:18 +0200

    oddanie

--- app/src/main/java/com/example/gpr_etd/MainActivity.java
+++ app/src/main/java/com/example/gpr_etd/MainActivity.java
@@ -4,35 +4,86 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
 import android.app.AppOpsManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Method;
 
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity{
     private Button btnOpenSettings;
+    private SwitchCompat switchCompatHotSpot;
+
+    private boolean isRestored;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnOpenSettings = (Button) findViewById(R.id.button);
-
-
-        btnOpenSettings.setOnClickListener(new View.OnClickListener() {
+        bindWidget();
+        /*switchCompatHotSpot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+                if (switchCompatHotSpot.isChecked()) {
+                    Toast.makeText(getApplicationContext(), "Switch HotSPOT is on: " + "true", LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Switch HotSPOT is on: " + "false", LENGTH_SHORT).show();
+                }
             }
-        });
+        });*/
 
+        //setupSwitchCompatEventListener();
+        setupWidgetListener();
+    }
+
+    private void setupSwitchCompatEventListener() {
+        //switchHotSpot.setOnCheckedChangeListener(this);
     }
 
 
 
+
+    private void setupWidgetListener() {
+        btnOpenSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(Settings.ACTION_WIRELESS_SETTINGS), 0);
+            }
+        });
+    }
+
+    private void bindWidget() {
+        btnOpenSettings = (Button) findViewById(R.id.btnOpenWirelessSettings);
+        switchCompatHotSpot = (SwitchCompat) findViewById(R.id.switchHotSpot);
+    }
+
+
+/*    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case (R.id.switchHotSpot):
+                if(isChecked) {
+                    isRestored = false;
+                    Toast.makeText(getApplicationContext(), "Switch HotSPOT is on: " + isChecked, LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Switch HotSPOT is on: " + isChecked, LENGTH_SHORT).show();
+
+                }
+                break;
+        }
+    }*/
 }
